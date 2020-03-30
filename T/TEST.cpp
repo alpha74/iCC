c95a1f171edddf2b55056da92825244f97d16354
Aman Kumar <amanalphakumar@gmail.com>
2020-03-30 22:27:25 +0530

    Create TEST.cpp

---
+++ T/TEST.cpp
@@ -0,0 +1,19 @@
+// Link: https://www.codechef.com/LRNDSA01/problems/TEST
+
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+  short int n = 1 ;
+  
+  while( n != 42 )
+  {
+    cin >> n ;
+    
+    if( n != 42 )
+      cout << "\n" << n ;
+  }
+  return 0 ;
+}
