ea61222f2cf5688b6206311708fac320e8f0ad51
Aman Kumar <amanalphakumar@gmail.com>
2020-04-02 18:04:22 +0530

    Create FLOW007.cpp

---
+++ F/FLOW007.cpp
@@ -0,0 +1,26 @@
+// Reverse the given number
+// https://www.codechef.com/LRNDSA01/problems/FLOW007
+
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+  int t ;
+  cin >> t ; 
+  
+  while( t-- )
+  {
+    long long int n, num = 0 ;
+    cin >> n ;
+    
+    while( n > 0 )
+    {
+      num = num*10 + n%10 ;
+      n /= 10 ;
+    }
+    cout << num << "\n" ;
+   }
+  
+}
