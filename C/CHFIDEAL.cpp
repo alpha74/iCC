9c7d8210fed35378357fba737a34d02851acf82d
Aman Kumar <amanalphakumar@gmail.com>
2020-07-20 01:21:44 +0530

    Create CHFIDEAL.cpp

---
+++ C/CHFIDEAL.cpp
@@ -0,0 +1,25 @@
+// Chef and An Ideal Problem
+// https://www.codechef.com/DEC18B/problems/CHFIDEAL
+// Aman Kumar
+
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main()
+{
+  int x = 1, y, z ;
+  
+  cout << x << "\n" ;
+  cin >> y ;
+  
+  if( y == 2 )
+    z = 3 ;
+  else
+    z = 2 ;
+    
+  cout << z << "\n" ;
+  fflush( stdin );
+  fflush( stdout ) ;   
+  
+}
